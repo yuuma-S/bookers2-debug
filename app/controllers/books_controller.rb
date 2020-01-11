5eac4a2946146f62ee912ba76323cd337f8d0d72
yuuma-S <y.sakurai2206@gmail.com>
2020-01-11 10:27:00 +0000

    destroy modify

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
-
   def show
+    @book_new = Book.new
   	@book = Book.find(params[:id])
     @user = current_user
   end
@@ -36,9 +36,9 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
